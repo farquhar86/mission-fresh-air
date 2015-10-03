1834475166d718d0288b76a4d229af93aaecb330
farquhar86 <farquharbradley@gmail.com>
2015-10-03 12:22:55 -0700

    Added the private and public key to stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'admin/index'
 
   root to: "welcome#index"
 
   get '/welcome/checkout', to: "welcome#checkout"
 
+  resources :charges
+
 
 
 end
+
+
+
+     #     charges GET    /charges(.:format)          charges#index
+     #             POST   /charges(.:format)          charges#create
+     #  new_charge GET    /charges/new(.:format)      charges#new
+     # edit_charge GET    /charges/:id/edit(.:format) charges#edit
+     #      charge GET    /charges/:id(.:format)      charges#show
+     #             PATCH  /charges/:id(.:format)      charges#update
+     #             PUT    /charges/:id(.:format)      charges#update
+     #             DELETE /charges/:id(.:format)      charges#destroy
