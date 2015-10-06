18ffa78b15fb326eb86f7daeab7583751eb0c19f
sakinasheikh <sfsakina@gmail.com>
2015-10-06 15:14:20 -0700

    added user information to list in back end

--- app/controllers/superusers_controller.rb
+++ app/controllers/superusers_controller.rb
@@ -3,5 +3,7 @@ class SuperusersController < ApplicationController
   before_action :require_login, only: [:show]
 
   def show
+  	@superuser = Superuser.find(1)
+  	@purchases = Purchase.all
   end
 end
