67d2ae4625578062f419731e31054f02e5319e9e
Marcel Degas <marcel.degas@gmail.com>
2015-10-06 18:34:34 -0700

    the welcome controller has tests and are passing wintergreen

--- db/migrate/20151002233623_create_adminemails.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdminemails < ActiveRecord::Migration
-  def change
-    create_table :adminemails do |t|
-      t.string :password_digest
-
-      t.timestamps null: false
-    end
-  end
-end
